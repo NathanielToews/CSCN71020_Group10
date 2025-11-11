13d52c5806b375abf48b8b06858ef96a075c35ef
Hir02-777 <marcus.tirbany@hotmail.com>
2025-11-11 12:12:42 -0500

    Linked unit test file to main and triangleSolver, and connected the analyze function

---
+++ Triangle Testing File/Triangle Testing File.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" char* analyzeTriangle(int side1, int side2, int side3);
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TriangleTestingFile
+{
+	TEST_CLASS(TriangleTestingFile)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
